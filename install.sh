e2bffdf3c2af5ed8bd10680be9381d2d70d3be38
Axu <axu@Axu-freemium.(none)>
2013-09-30 16:01:48 +0300

    Add ROSARIA to install script.

--- install.sh
+++ install.sh
@@ -10,3 +10,7 @@ rosws init ~/fuerte_workspace /opt/ros/fuerte
 source ~/fuerte_workspace/setup.bash
 rosws set fsr2013 --git https://github.com/purplenavi/FSR13Group2.git
 rosws update fsr2013
+rosws set amor-ros-pkg https://code.google.com/p/amor-ros-pkg/ --hg -v fuerte
+rosws update amor-ros-pkg
+source ~/fuerte_workspace/setup.bash
+rosmake ROSARIA
