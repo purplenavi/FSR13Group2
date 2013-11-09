c15f430acb9dc4dd867c0658d0aba0892847808f
Aleksi Salonen <aleksi.m.salonen@aalto.fi>
2013-11-09 11:40:27 +0200

    Kinect bag and also modifications to detecting pirates

---
+++ hiekkalaatikko/imge_recognition/build/CMakeFiles/ppl_recognition.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/ppl_recognition.dir/src/ppl_recognition.o"
+  "../bin/ppl_recognition.pdb"
+  "../bin/ppl_recognition"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/ppl_recognition.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
