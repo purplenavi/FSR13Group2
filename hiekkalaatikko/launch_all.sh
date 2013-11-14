af9953be0554d1c3752deaf38441fe186614c692
Aleksi Salonen <aleksi.m.salonen@aalto.fi>
2013-11-14 04:14:33 +0200

    VMP, ei toimi

--- hiekkalaatikko/launch_all.sh
+++ hiekkalaatikko/launch_all.sh
@@ -4,5 +4,4 @@ roslaunch gui_plannerz.launch &
 cd ~/fuerte_workspace/fsr2013/hiekkalaatikko/point2point_navigation
 bin/point2point_navigation &
 cd ~/fuerte_workspace/fsr2013/hiekkalaatikko/gui_plannerz/src
-#./gui_plannerz.py &
-./task_plannerz.py &
+./manipulator_competition_gui.py &
